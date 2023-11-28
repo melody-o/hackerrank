e2621424547c2e627f765b17dc29620e06045913
Melody <melodyoberg@icloud.com>
2023-11-28 15:46:54 -0800

    solvethisfirst

---
+++ algorithms/javascript/solveThisFirst.js
@@ -0,0 +1,16 @@
+//Complete the function solveMeFirst to compute the sum of two integers.
+
+//Function Description
+
+//Complete the solveMeFirst function in the editor below.
+
+//solveMeFirst has the following parameters:
+
+//int a: the first value
+//int b: the second value
+//Returns
+//- int: the sum of  and
+
+function solveMeFirst(a, b) {
+    return a + b
+  }
