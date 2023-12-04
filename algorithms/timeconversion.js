8d7bcfe67d340c7cb60f228dd9a6b8ee27d57f03
Justin <burgoniojustin@yahoo.com>
2023-12-04 12:15:45 -0800

    time conversion

--- algorithms/timeconversion.js
+++ algorithms/timeconversion.js
@@ -1,3 +1,34 @@
+// Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+
+// Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+// - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+// Example
+
+
+// Return '12:01:00'.
+
+
+// Return '00:01:00'.
+
+// Function Description
+
+// Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+
+// timeConversion has the following parameter(s):
+
+// string s: a time in  hour format
+// Returns
+
+// string: the time in  hour format
+// Input Format
+
+// A single string  that represents a time in -hour clock format (i.e.:  or ).
+
+
+
+
+
 function timeConversion(s) {
     // Write your code here
     //split our string to check if last two are AM or PM
@@ -40,4 +71,21 @@ function timeConversion(s) {
 
 
 
+// function timeConversion(s) {
+//     const period = s.slice(-2);
+//     let hour = parseInt(s.slice(0, 2));
 
+//     if (period === "PM" && hour !== 12) {
+//         hour += 12;
+//     } else if (period === "AM" && hour === 12) {
+//         hour = 0;
+//     }
+
+//     if (hour < 10) {
+//         hour = "0" + hour;
+//     }
+
+//     const result = hour + s.slice(2, -2);
+
+//     return result;
+// }
