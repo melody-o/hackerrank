0306f43f5be75e778ce15e473f29a3b21bc3c67c
Justin <burgoniojustin@yahoo.com>
2023-12-08 09:54:25 -1000

    description & input

--- algorithms/javascript/appleAndOrange.js
+++ algorithms/javascript/appleAndOrange.js
@@ -9,3 +9,53 @@
 // Given the value of  for  apples and  oranges, determine how many apples and oranges will fall on Sam's house (i.e., in the inclusive range )?
 
 // For example, Sam's house is between  and . The apple tree is located at  and the orange at . There are  apples and  oranges. Apples are thrown  units distance from , and  units distance. Adding each apple distance to the position of the tree, they land at . Oranges land at . One apple and two oranges land in the inclusive range  so we print
+
+
+// Function Description
+
+// Complete the countApplesAndOranges function in the editor below. It should print the number of apples and oranges that land on Sam's house, each on a separate line.
+
+// countApplesAndOranges has the following parameter(s):
+
+// s: integer, starting point of Sam's house location.
+// t: integer, ending location of Sam's house location.
+// a: integer, location of the Apple tree.
+// b: integer, location of the Orange tree.
+// apples: integer array, distances at which each apple falls from the tree.
+// oranges: integer array, distances at which each orange falls from the tree.
+// Input Format
+
+// The first line contains two space-separated integers denoting the respective values of  and .
+// The second line contains two space-separated integers denoting the respective values of  and .
+// The third line contains two space-separated integers denoting the respective values of  and .
+// The fourth line contains  space-separated integers denoting the respective distances that each apple falls from point .
+// The fifth line contains  space-separated integers denoting the respective distances that each orange falls from point .
+
+// Constraints
+
+// Output Format
+
+// Print two integers on two different lines:
+
+// The first integer: the number of apples that fall on Sam's house.
+// The second integer: the number of oranges that fall on Sam's house.
+// Sample Input 0
+
+// 7 11
+// 5 15
+// 3 2
+// -2 2 1
+// 5 -6
+// Sample Output 0
+
+// 1
+// 1
+// Explanation 0
+
+// The first apple falls at position .
+// The second apple falls at position .
+// The third apple falls at position .
+// The first orange falls at position .
+// The second orange falls at position .
+// Only one fruit (the second apple) falls within the region between  and , so we print  as our first line of output.
+// Only the second orange falls within the region between  and , so we print  as our second line of output.
