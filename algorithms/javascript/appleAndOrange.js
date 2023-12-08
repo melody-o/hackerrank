4ce623e59342b49ef8b464a39d9d26a562a2b9ab
Justin <burgoniojustin@yahoo.com>
2023-12-08 09:53:44 -1000

    question

--- algorithms/javascript/appleAndOrange.js
+++ algorithms/javascript/appleAndOrange.js
@@ -5,3 +5,7 @@
 // The red region denotes the house, where  is the start point, and  is the endpoint. The apple tree is to the left of the house, and the orange tree is to its right.
 // Assume the trees are located on a single point, where the apple tree is at point , and the orange tree is at point .
 // When a fruit falls from its tree, it lands  units of distance from its tree of origin along the -axis. *A negative value of  means the fruit fell  units to the tree's left, and a positive value of  means it falls  units to the tree's right. *
+
+// Given the value of  for  apples and  oranges, determine how many apples and oranges will fall on Sam's house (i.e., in the inclusive range )?
+
+// For example, Sam's house is between  and . The apple tree is located at  and the orange at . There are  apples and  oranges. Apples are thrown  units distance from , and  units distance. Adding each apple distance to the position of the tree, they land at . Oranges land at . One apple and two oranges land in the inclusive range  so we print
