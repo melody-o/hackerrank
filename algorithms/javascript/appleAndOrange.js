4d70b3347ccc47af461d5bbdf51eb75f5a854a0a
Justin <burgoniojustin@yahoo.com>
2023-12-15 13:25:55 -0800

    apples and oranges

--- algorithms/javascript/appleAndOrange.js
+++ algorithms/javascript/appleAndOrange.js
@@ -59,3 +59,32 @@
 // The second orange falls at position .
 // Only one fruit (the second apple) falls within the region between  and , so we print  as our first line of output.
 // Only the second orange falls within the region between  and , so we print  as our second line of output.
+
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    // Write your code here
+    //result array
+    //a counter
+    //b counter
+    //iterate through apples
+    //check to see if a + apples[i]  >= s && a <= t
+    //a counter++
+    //if b - oranges[i] <= t && >= s
+    //b counter++
+    //push counters to result array and return result
+    let arr = [];
+    let apps = 0;
+    let oj = 0;
+    for (let i = 0; i <apples.length; i++) {
+        if (a + apples[i] >= s && a + apples[i] <= t) {
+            apps++
+            arr.push(apps)
+        }
+    }
+    for (let j = 0; j <oranges.length; j++) {
+        if (b + oranges[j] <= t && b + oranges[j] >= s) {
+            oj++
+            arr.push(oj)
+        }
+    }
+    return arr;
+}
